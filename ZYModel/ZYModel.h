3b2211b93e8346a55734251df18c38408427ec57
刘子洋 <sheepliu@sheepliu-mc1.local>
2016-03-29 15:53:38 +0800

    Initial Commit

---
+++ ZYModel/ZYModel.h
@@ -0,0 +1,19 @@
+//
+//  ZYModel.h
+//  ZYModel
+//
+//  Created by sheepliu on 16/3/29.
+//  Copyright © 2016年 tzuyangliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZYModel.
+FOUNDATION_EXPORT double ZYModelVersionNumber;
+
+//! Project version string for ZYModel.
+FOUNDATION_EXPORT const unsigned char ZYModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZYModel/PublicHeader.h>
+
+
